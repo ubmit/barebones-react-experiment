fc7dde29ca3cce82520faf07f03a7310c5f39acb
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2023-02-07 21:06:54 +0000

    add scripts

--- public/index.js
+++ public/index.js
@@ -2,9 +2,31 @@
 
 var _react = _interopRequireDefault(require("react"));
 var _reactDom = require("react-dom");
+var _jsxFileName = "/Users/guilherme/foo/src/index.js";
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function App() {
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World!");
+  return /*#__PURE__*/_react["default"].createElement("h1", {
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 10
+    }
+  }, "Hello World!");
 }
 var root = (0, _reactDom.createRoot)(document.getElementById('root'));
-root.render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(App, null)));
+root.render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, {
+  __self: void 0,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 10,
+    columnNumber: 3
+  }
+}, /*#__PURE__*/_react["default"].createElement(App, {
+  __self: void 0,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 11,
+    columnNumber: 5
+  }
+})));
